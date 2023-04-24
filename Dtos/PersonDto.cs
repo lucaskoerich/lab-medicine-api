63ff6ea5315ccc2db1a426f0679c7f83b09a62f4
lucaskoerich <lucaskoerichsouza@hotmail.com>
2023-04-24 13:35:04 -0300

    remove column annotation

--- Dtos/PersonDto.cs
+++ Dtos/PersonDto.cs
@@ -5,25 +5,20 @@ namespace lab_medicine_api.Dtos;
 
 public abstract class PersonDto
 {
-    [Column("NAME")]
     [Required(ErrorMessage = "Nome não pode ser vazio!")]
     [MinLength(5, ErrorMessage = "Nome precisa conter no mínimo 5 caracteres.")]
     public string Name { get; set; }
-    
-    [Column("GENDER")]
+
     [Required(AllowEmptyStrings = false, ErrorMessage = "Gênero não pode ser vazio!")]
     public string Gender { get; set; }
 
-    [Column("BIRTH_DATE")]
     [Required(ErrorMessage = "Deve ser uma data válida.")]
     public DateTime BirthDate { get; set; }
 
-    [Column("CPF")]
     [Required(ErrorMessage = "CPF não pode ser vazio!")]
     [RegularExpression(@"^\d{11}$", ErrorMessage = "CPF deve conter apenas 11 dígitos numéricos.")]
     public string CPF { get; set; }
 
-    [Column("PHONE_NUMBER")]
     [Required(ErrorMessage = "Telefone não pode ser vazio!")]
     [MinLength(11, ErrorMessage = "Telefone deve conter pelo menos 11 caracteres.")]
     public string PhoneNumber { get; set; }
@@ -31,5 +26,5 @@ public abstract class PersonDto
 
 public class GetPersonDto : PersonDto
 {
-    [Column("ID")][Key] public int Id { get; set; }
+    [Key] public int Id { get; set; }
 }
